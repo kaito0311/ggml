ca4a0c3abc73597b05498de2e4bbb2b6eb4316dc
kaito0311 <tanminh8b@gmail.com>
2025-02-12 00:00:51 +0700

    .

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 cmake -DGGML_CUDA=ON -DCMAKE_CUDA_COMPILER=/usr/local/cuda-11.7/bin/nvcc -B build
-cmake --build build --config Release --target demo
+cmake --build build --config Release --target demo -j 8 
 
 # Run it
 ./build/bin/demo
--- examples/demo/demo.cpp
+++ examples/demo/demo.cpp
@@ -1,118 +1,218 @@
 #include "ggml.h"
+#include "ggml-cpu.h"
 #include "ggml-alloc.h"
 #include "ggml-backend.h"
+
 #ifdef GGML_USE_CUDA
 #include "ggml-cuda.h"
 #endif
 
-#include "ggml-cpu.h"
+#ifdef GGML_USE_METAL
+#include "ggml-metal.h"
+#endif
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
 
+static void ggml_log_callback_default(ggml_log_level level, const char * text, void * user_data) {
+    (void) level;
+    (void) user_data;
+    fputs(text, stderr);
+    fflush(stderr);
+}
+
+// This is a simple model with two tensors a and b
+struct simple_model {
+    struct ggml_tensor * a;
+    struct ggml_tensor * b;
+    struct ggml_tensor * c;
+
+    // the backend to perform the computation (CPU, CUDA, METAL)
+    ggml_backend_t backend = NULL;
+
+    // the backend buffer to storage the tensors data of a and b
+    ggml_backend_buffer_t buffer;
+
+    // the context to define the tensor information (dimensions, size, memory address)
+    struct ggml_context * ctx;
+};
+
+// initialize the tensors of the model in this case two matrices 2x2
+void load_model(simple_model & model, float * a, float * b, float * c, int rows_A, int cols_A, int rows_B, int cols_B, int rows_C, int cols_C) {
+    ggml_log_set(ggml_log_callback_default, nullptr);
+
+    // initialize the backend
+#ifdef GGML_USE_CUDA
+    fprintf(stderr, "%s: using CUDA backend\n", __func__);
+    model.backend = ggml_backend_cuda_init(0); // init device 0
+    if (!model.backend) {
+        fprintf(stderr, "%s: ggml_backend_cuda_init() failed\n", __func__);
+    }
+#endif
+
+#ifdef GGML_USE_METAL
+    fprintf(stderr, "%s: using Metal backend\n", __func__);
+    model.backend = ggml_backend_metal_init();
+    if (!model.backend) {
+        fprintf(stderr, "%s: ggml_backend_metal_init() failed\n", __func__);
+    }
+#endif
+
+    // if there aren't GPU Backends fallback to CPU backend
+    if (!model.backend) {
+        model.backend = ggml_backend_cpu_init();
+    }
+
+    int num_tensors = 3;
+
+    struct ggml_init_params params {
+            /*.mem_size   =*/ ggml_tensor_overhead() * num_tensors,
+            /*.mem_buffer =*/ NULL,
+            /*.no_alloc   =*/ true,
+    };
+
+    // create context
+    model.ctx = ggml_init(params);
+
+    // create tensors
+    model.a = ggml_new_tensor_2d(model.ctx, GGML_TYPE_F32, cols_A, rows_A);
+    model.b = ggml_new_tensor_2d(model.ctx, GGML_TYPE_F32, cols_B, rows_B);
+    model.c = ggml_new_tensor_2d(model.ctx, GGML_TYPE_F32, cols_C, rows_C);
+
+    // create a backend buffer (backend memory) and alloc the tensors from the context
+    model.buffer = ggml_backend_alloc_ctx_tensors(model.ctx, model.backend);
+
+    // load data from cpu memory to backend buffer
+    ggml_backend_tensor_set(model.a, a, 0, ggml_nbytes(model.a));
+    ggml_backend_tensor_set(model.b, b, 0, ggml_nbytes(model.b));
+    ggml_backend_tensor_set(model.c, c, 0, ggml_nbytes(model.c));
+}
+
+// build the compute graph to perform a matrix multiplication
+struct ggml_cgraph * build_graph(const simple_model& model) {
+    static size_t buf_size = ggml_tensor_overhead()*GGML_DEFAULT_GRAPH_SIZE + ggml_graph_overhead();
+    static std::vector<uint8_t> buf(buf_size);
+
+    struct ggml_init_params params0 = {
+        /*.mem_size   =*/ buf_size,
+        /*.mem_buffer =*/ buf.data(),
+        /*.no_alloc   =*/ true, // the tensors will be allocated later by ggml_allocr_alloc_graph()
+    };
+
+    // create a temporally context to build the graph
+    struct ggml_context * ctx0 = ggml_init(params0);
+
+    struct ggml_cgraph  * gf = ggml_new_graph(ctx0);
+
+    // result = a*b^T
+    struct ggml_tensor * result = ggml_mul_mat(ctx0, model.a, model.b);
+    struct ggml_tensor * result2 = ggml_mul_mat(ctx0, result, model.c);
+    
+    // build operations nodes
+    // ggml_build_forward_expand(gf, result);
+    ggml_build_forward_expand(gf, result2);
+
+    // Print the cgraph
+    ggml_graph_print(gf);
+
+    ggml_graph_dump_dot(gf, NULL, "debug.dot");
+
+    // delete the temporally context used to build the graph
+    ggml_free(ctx0);
+    return gf;
+}
+
+// compute with backend
+struct ggml_tensor * compute(const simple_model & model, ggml_gallocr_t allocr) {
+    // reset the allocator to free all the memory allocated during the previous inference
+
+    struct ggml_cgraph * gf = build_graph(model);
+
+    // allocate tensors
+    ggml_gallocr_alloc_graph(allocr, gf);
+
+    int n_threads = 1; // number of threads to perform some operations with multi-threading
+
+    if (ggml_backend_is_cpu(model.backend)) {
+        ggml_backend_cpu_set_n_threads(model.backend, n_threads);
+    }
+
+    ggml_backend_graph_compute(model.backend, gf);
+
+    // in this case, the output tensor is the last one in the graph
+    return ggml_graph_node(gf, 0);
+}
 
 int main(void) {
+    ggml_time_init();
+
     // initialize data of matrices to perform matrix multiplication
     const int rows_A = 4, cols_A = 2;
+
     float matrix_A[rows_A * cols_A] = {
         2, 8,
         5, 1,
         4, 2,
         8, 6
     };
+
     const int rows_B = 3, cols_B = 2;
+    /* Transpose([
+        10, 9, 5,
+        5, 9, 4
+    ]) 2 rows, 3 cols */
     float matrix_B[rows_B * cols_B] = {
         10, 5,
         9, 9,
         5, 4
     };
 
-    // 1. Initialize backend 
-    ggml_backend_t backend = NULL; 
+    const int rows_C = 3, cols_C = 4;
 
-#ifdef GGML_USE_CUDA
-    fprintf(stderr, "%s: using CUDA backend\n", __func__);
-    backend = ggml_backend_cuda_init(0); // init device 0
-    if (!backend) {
-        fprintf(stderr, "%s: ggml_backend_cuda_init() failed\n", __func__);
-    }
-#endif 
-
-    // if there aren't GPU Backends fallback to CPU backend
-    if (!backend) {
-        printf("Init cpu backend");
-        backend = ggml_backend_cpu_init();
-    }
-
-    // Calculate the size needed to allocate 
-    size_t ctx_size = 0;
-    ctx_size += 2 * ggml_tensor_overhead(); // tensors 
-
-    printf("ctx size: %ld", ctx_size);
-    // no need to allocate anything else! 
-
-    // 2. Allocate `ggml_context` to store tensor data 
-    struct ggml_init_params params = {
-        /*.mem_size   =*/ ctx_size, 
-        /*.mem_buffer =*/ NULL,
-        /*.no_alloc   =*/true, // the tensors will be allocated later by ggml_backend_alloc_ctx_tensors()
+    float matrix_C[rows_C * cols_C] = {
+        2, 2, 2,
+        2, 2, 2,
+        2, 2, 2,
+        2, 2, 2
     };
 
-    struct ggml_context * ctx = ggml_init(params);
+    simple_model model;
+    load_model(model, matrix_A, matrix_B, matrix_C, rows_A, cols_A, rows_B, cols_B, rows_C, cols_C);
 
-    struct ggml_tensor * tensor_a = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, cols_A, rows_A);
-    struct ggml_tensor * tensor_b = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, cols_B, rows_B);
-
-
-    ggml_backend_buffer_t buffer = ggml_backend_alloc_ctx_tensors(ctx, backend);
-
-    ggml_backend_tensor_set(tensor_a, matrix_A, 0, ggml_nbytes(tensor_a));
-    ggml_backend_tensor_set(tensor_b, matrix_B, 0, ggml_nbytes(tensor_b));
-
-
-    struct ggml_cgraph * gf = NULL;
-    struct ggml_context * ctx_graph = NULL; 
+    // calculate the temporaly memory required to compute
+    ggml_gallocr_t allocr = NULL;
 
     {
-        // create a temp context to build the graph 
-        struct ggml_init_params params0 = {
-            /*.mem_size   =*/ ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + ggml_tensor_overhead(),
-            /*.mem_buffer =*/ NULL,
-            /*.no_alloc   =*/ true, // the tensors will be allocated later by ggml_backend_alloc_ctx_tensors()
-        };
+        allocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(model.backend));
 
-        ctx_graph = ggml_init(params0);
-        gf = ggml_new_graph(ctx_graph);
+        // create the worst case graph for memory usage estimation
+        struct ggml_cgraph * gf = build_graph(model);
+        ggml_gallocr_reserve(allocr, gf);
+        size_t mem_size = ggml_gallocr_get_buffer_size(allocr, 0);
 
-        // result = a * b^T
-        // pay attention = ggml_mat_mul(A, B) ==> B will be tranposed internally 
-        // the result is tranposed 
-        struct ggml_tensor * result0 = ggml_mul_mat(ctx_graph, tensor_a, tensor_b);
-
-        // add "result" tensor and all of its dependencies to the cgraph 
-        ggml_build_forward_expand(gf, result0); 
-
+        fprintf(stderr, "%s: compute buffer size: %.4f KB\n", __func__, mem_size/1024.0);
     }
 
+    // perform computation
+    struct ggml_tensor * result = compute(model, allocr);
 
-    ggml_gallocr_t allocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(backend));
-    ggml_gallocr_alloc_graph(allocr, gf);
+    // create a array to print result
+    std::vector<float> out_data(ggml_nelements(result));
 
-    int n_threads = 1;
+    // bring the data from the backend memory
+    ggml_backend_tensor_get(result, out_data.data(), 0, ggml_nbytes(result));
 
-    if (ggml_backend_is_cpu(backend)) {
-        ggml_backend_cpu_set_n_threads(backend, n_threads);
-    }
+    // expected result:
+    // [ 60.00 110.00 54.00 29.00
+    //  55.00 90.00 126.00 28.00
+    //  50.00 54.00 42.00 64.00 ]
 
-    ggml_backend_graph_compute(backend, gf);
-
-    // 10. Retrieve result 
-    // in this example output tensor is always the last tensor in the graph 
-    struct ggml_tensor * result = ggml_graph_node(gf, -1);
-    float * result_data = (float*)malloc(ggml_nbytes(result));
-
-    ggml_backend_tensor_get(result, result_data, 0, ggml_nbytes(result));
     printf("mul mat (%d x %d) (transposed result):\n[", (int) result->ne[0], (int) result->ne[1]);
     for (int j = 0; j < result->ne[1] /* rows */; j++) {
         if (j > 0) {
@@ -120,16 +220,19 @@ int main(void) {
         }
 
         for (int i = 0; i < result->ne[0] /* cols */; i++) {
-            printf(" %.2f", result_data[j * result->ne[0] + i]);
+            printf(" %.2f", out_data[i * result->ne[1] + j]);
         }
     }
     printf(" ]\n");
-    free(result_data);
 
-    ggml_free(ctx_graph);
+    // release backend memory used for computation
     ggml_gallocr_free(allocr);
-    ggml_free(ctx);
-    ggml_backend_buffer_free(buffer);
-    ggml_backend_free(backend);
+
+    // free memory
+    ggml_free(model.ctx);
+
+    // release backend memory and free backend
+    ggml_backend_buffer_free(model.buffer);
+    ggml_backend_free(model.backend);
     return 0;
 }
